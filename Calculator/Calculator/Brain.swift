cb4a3f0b98afab51aa81e58d75c237fee7dc2a03
Alice Gamarnik <agamarnik1@pride.hofstra.edu>
2019-03-04 14:40:25 -0500

    Fixed constraints

---
+++ Calculator/Calculator/Brain.swift
@@ -0,0 +1,18 @@
+//
+//  Brain.swift
+//  Calculator
+//
+//  Created by Alice Gamarnik on 3/4/19.
+//  Copyright © 2019 Alice Gamarnik. All rights reserved.
+//
+
+import Foundation
+class Brain {
+    var display:String
+    init(){
+        display = "0"
+    }
+    func evaluate(operator: String){
+        
+    }
+}

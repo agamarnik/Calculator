255a4029d8c757571622b5b9dbede9c932c67893
Alice Gamarnik <agamarnik1@pride.hofstra.edu>
2019-03-04 14:08:24 -0500

    Constraints for buttons

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -12,9 +12,16 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var resultLabel: UILabel!
     
+    var firstNumberText = ""
+    var secondNumberText = ""
+    var op = ""
+    var isFirstNumber = true
+    var hasOp = false
+    var canClear = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        resultLabel.text = "0"
+        resultLabel.text = ""
     }
     
     override func didReceiveMemoryWarning() {
@@ -23,14 +30,62 @@ class ViewController: UIViewController {
     }
     
     @IBAction func btnNumberClicked(_ sender: UIButton) {
-        resultLabel.text = sender.currentTitle
+        //resultLabel.text = sender.currentTitle
+        let currentText = resultLabel.text!
+        let textLabel = sender.titleLabel?.text
+        if let text = textLabel {
+            switch text {
+            case "+", "*", "/", "-":
+                if hasOp {
+                    return
+                }
+                op = text
+                isFirstNumber = false
+                hasOp = true
+                resultLabel.text = "\(currentText) \(op) "
+                break
+            case "=":
+                isFirstNumber = true
+                hasOp = false
+                canClear = true
+                let result = calculate()
+                resultLabel.text = "\(result)"
+                break
+            default:
+                if isFirstNumber {
+                    firstNumberText = "\(firstNumberText)\(text)"
+                } else {
+                    secondNumberText = "\(secondNumberText)\(text)"
+                }
+                resultLabel.text = "\(currentText)\(text)"
+                break;
+            }
+        }
     }
     
     @IBAction func acPressed(_ sender: UIButton) {
         resultLabel.text = "0"
+        canClear = true
     }
     
-    
+    func calculate() -> Double {
+        let firstNumber = Double(firstNumberText)!
+        let secondNumber = Double(secondNumberText)!
+        firstNumberText = ""
+        secondNumberText = ""
+        switch op {
+        case "+":
+            return firstNumber + secondNumber
+        case "-":
+            return firstNumber - secondNumber
+        case "*":
+            return firstNumber * secondNumber
+        case "/":
+            return firstNumber / secondNumber
+        default:
+            return 0
+        }
+    }
     
     
 
